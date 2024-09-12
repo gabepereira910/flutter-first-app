8e22be95ce2b363ab07005e35d18d17cb98ecedb
Gabriel Pereira <gabe.pereira910@gmail.com>
2024-09-12 19:14:42 -0300

    Primeira versao do primeiro projeto em FLUTTER

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: Center(
+          child: Text('Terceira aula do Flutter'),
+        ),
+      ),
+    );
+  }
+}
+
